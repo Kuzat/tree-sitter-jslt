2a8edde2df2f117d6a21ffa7cb80a05d97698115
Vegard Bjørsvik <vegard.bjorsvik@schibsted.com>
2023-08-25 14:46:16 +0200

    inital highlights.scm

---
+++ queries/highlights.scm
@@ -0,0 +1,29 @@
+
+(variable) @variable 
+
+(string) @string
+
+[
+ (integer)
+ (decimal)
+]@number
+
+[
+ (null)
+ (true)
+ (false)
+] @constant.builtin
+
+(escape_sequence) @escape
+
+(single_line_comment) @comment
+
+[
+  (as)
+  (let)
+  (def)
+  (if)
+  (else)
+  (import)
+  (for)
+] @keyword
